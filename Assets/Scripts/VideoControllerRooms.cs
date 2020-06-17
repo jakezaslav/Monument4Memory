b4e4fa84b2520654d5f34176cb2b66b1bf92d440
jakezaslav <jake.zaslav@gmail.com>
2020-06-17 12:45:40 -0400

    Organization
    Organized the assets and scene a little better which will make it easier for us to move things around the space

---
+++ Assets/Scripts/VideoControllerRooms.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoControllerRooms : MonoBehaviour
+{
+
+    public GameObject video;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        video.SetActiveRecursively(true);
+
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        video.SetActiveRecursively(false);
+    }
+
+}
